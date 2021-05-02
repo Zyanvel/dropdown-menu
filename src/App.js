ae7c7b17c62c16c6ea625854acbc9aa1b158085c
Zyanvel <zyanvel@gmail.com>
2021-05-02 10:44:56 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.scss';
+import Dropdown from './Dropdown';
+
+const pokemons = [
+  {
+    id: 1,
+    value: 'Pikachu',
+  },
+  {
+    id: 2,
+    value: 'Piplup',
+  },
+  {
+    id: 3,
+    value: 'Eevee',
+  },
+  {
+    id: 4,
+    value: 'Chikorita',
+  },
+  {
+    id: 5,
+    value: 'Milotic',
+  },
+  {
+    id: 6,
+    value: 'Flygon',
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Zyan's favorite pokemons :D{' '}
+      </h1>
+      <Dropdown title="Select pokemon" items={pokemons} multiSelect />
+    </div>
+  );
+}
+
+export default App;
